be57763d79c3967d77d5f8fce1de73990acc998e
Xavier <xajouppe@gmail.com>
2016-07-09 00:58:37 +0200

    add powerline fonts

--- setup_dev_config.sh
+++ setup_dev_config.sh
@@ -1,5 +1,17 @@
+
+# Oh-my-zsh
 sudo apt-get install zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+mv oh-my-zsh/.zshrc ~/
+
+# powerline
+wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
+wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mv PowerlineSymbols.otf ~/.fonts/
+fc-cache -vf ~/.fonts/
+mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+# vim
 mv vim/.vim/ ~/
 mv vim/.vimrc ~/ 
-mv vim/oh-my-zsh/.zshrc ~/
+
