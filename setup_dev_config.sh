f2a7e7da9e0a98d1fa1d1383908e09938082199e
Xavier <xajouppe@gmail.com>
2016-07-09 00:50:30 +0200

    add zsh install

--- setup_dev_config.sh
+++ setup_dev_config.sh
@@ -1,4 +1,5 @@
+sudo apt-get install zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-mv .vim/ ~/
-mv .vimrc ~/ 
-mv oh-my-zsh/.zshrc ~/
+mv vim/.vim/ ~/
+mv vim/.vimrc ~/ 
+mv vim/oh-my-zsh/.zshrc ~/
