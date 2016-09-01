27754654e80c268f42643f0ab9ed9da7f7b0fd2c
XavierJp <xajouppe@gmail.com>
2016-09-01 11:39:38 +0200

    Pathogen plugin

---
+++ vim/pathogen_plugin.sh
@@ -0,0 +1,24 @@
+" Pathogen
+
+ctrlp
+
+pathogen
+
+colorizer
+
+autopairs
+
+indetline
+
+vim-airline
+
+vim-trailing-whitespace
+
+vim-airline-theme
+
+syntastic
+
+nerdtree
+
+javascript-libraries-syntax.vim
+
